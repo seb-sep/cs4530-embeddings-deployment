5b032f117a285dcd338f34dd7c33522615754b78
seb-sep <59621473+seb-sep@users.noreply.github.com>
2025-04-07 12:38:55 -0400

    functional modal endpoint w/embeddings

--- run.py
+++ run.py
@@ -1,18 +1,68 @@
 import modal
+from pathlib import Path
+from typing import List, Dict, Union
+import torch
+from transformers import AutoTokenizer, AutoModel
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 
-app = modal.App("example-get-started")
 
-image = modal.Image.debian_slim().pip_install("fastapi[standard]")
+class EmbeddingRequest(BaseModel):
+    texts: List[str]
+    is_query: bool = True
 
 
-@app.function(image=image)
+class EmbeddingResponse(BaseModel):
+    embeddings: List[List[float]]
+
+
+app = modal.App("fstackoverflow-embeddings")
+
+volume = modal.Volume.from_name("model-weights-vol", create_if_missing=True)
+MODEL_DIR = Path("/models")
+MODEL_NAME = "Snowflake/snowflake-arctic-embed-m-long"
+
+image = modal.Image.debian_slim().pip_install(
+    "fastapi[standard]", "transformers", "torch", "einops"
+)
+
+
+@app.function(image=image, gpu="T4", volumes={str(MODEL_DIR): volume})
 @modal.fastapi_endpoint()
-def square(x):
-    print("This code is running on a remote worker!")
-    x = int(x)
-    return x**2
+async def get_embeddings(request: EmbeddingRequest) -> EmbeddingResponse:
+    """Generate embeddings for a list of texts using Arctic Embeddings model.
 
+    Args:
+        request: Embedding request containing texts and configuration
 
-@app.local_entrypoint()
-def main():
-    print("the square is", square.remote(42))
+    Returns:
+        Embedding response containing list of embeddings
+
+    """
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    model = AutoModel.from_pretrained(
+        MODEL_NAME,
+        trust_remote_code=True,
+        add_pooling_layer=False,
+        safe_serialization=True,
+    )
+    model.eval()
+
+    texts = request.texts
+    # the HF docs reccomend prefixing this specifically to queries
+    # https://huggingface.co/Snowflake/snowflake-arctic-embed-m-long#usage
+    if request.is_query:
+        query_prefix = "Represent this sentence for searching relevant passages: "
+        texts = [f"{query_prefix}{text}" for text in texts]
+
+    tokens = tokenizer(
+        texts, padding=True, truncation=True, return_tensors="pt", max_length=512
+    )
+
+    with torch.no_grad():
+        embeddings = model(**tokens)[0][:, 0]
+
+    # Normalize embeddings
+    embeddings = torch.nn.functional.normalize(embeddings, p=2, dim=1)
+
+    return EmbeddingResponse(embeddings=embeddings.tolist())
